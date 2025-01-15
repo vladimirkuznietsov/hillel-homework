914de2623b253d404891dbcfc668b4751676d4de
vladimirkuznietsov <vladimir.kuznietsov.02@gmail.com>
2025-01-15 19:45:34 +0200

    Fix for hw5/task3

--- homework_5/js/task_3.js
+++ homework_5/js/task_3.js
@@ -1,7 +1,10 @@
 let number = prompt('Введіть ціле число:');
 
-if (isNaN(Number(number)) || number == null) {
+if (isNaN(Number(number))) {
     console.log('Введене значення не є числом');
+}
+else if (number == null || number == "") {
+    console.log('Значення не було введено');
 } else {
     for (let i = 1; i <= 100; i++) {
         if (i**2 > number) {
