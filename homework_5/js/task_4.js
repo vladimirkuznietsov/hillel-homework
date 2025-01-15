fc0b7c78c52adb0658abdf80e798643bd57c1a59
vladimirkuznietsov <vladimir.kuznietsov.02@gmail.com>
2025-01-15 23:34:56 +0200

    Fix for hw5/task 4

--- homework_5/js/task_4.js
+++ homework_5/js/task_4.js
@@ -1,10 +1,24 @@
 let number = prompt('Введіть число:');
 
-if (number > 1 && (number -1) % 6 == 0 || (number + 1) % 6 == 0 || number == 3 || number == 2) {
-    alert(`Число ${number} є простим`);
+if (Number(isNaN(number)) || number == "" || number == null) {
+    console.log('Число не було введено');
 }
-else if (isNaN(Number(number)) || number == null || number == "") {
-    alert('Число не було введено');
+else if (number < 2) {
+    console.log(`Число ${number} не є простим`);
+}
+else if (number == 2) {
+    console.log(`Число ${number} є простим`);
 } else {
-    alert(`Число ${number} не є простим`);
-};
+    let isPrime = true;
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(`Число ${number} є простим`);
+    } else {
+        console.log(`Число ${number} не є простим`);
+    }
+}
