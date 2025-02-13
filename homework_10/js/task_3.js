fa7cc499256cf1f3198d253ca7b2147d27a886c3
vladimirkuznietsov <vladimir.kuznietsov.02@gmail.com>
2025-02-13 20:07:58 +0200

    hw10/task3

---
+++ homework_10/js/task_3.js
@@ -0,0 +1,39 @@
+const contactBook = {
+    data: [
+        {
+            name: 'John',
+            phoneNumber: '123456',
+            email: 'kent@gmail.com'
+        },
+        {
+            name: 'Sam',
+            phoneNumber: '54321',
+            email: 'kent2@gmail.com'
+        },
+        {
+            name: 'Dave',
+            phoneNumber: '76589',
+            email: 'kent3@gmail.com'
+        }
+    ],
+    searchContact: function(name) {
+        result = this.data.find(contact => contact.name.toLowerCase() === name.toLowerCase());
+        if(result) {
+            return `Name: ${result.name}
+                    \nPhone Number: ${result.phoneNumber}
+                    \nEmail: ${result.email}`;
+        } else {
+            return 'No results';
+        }
+    },
+    addContact: function(name, number, email) {
+        const newContact = {name: name,
+                            phoneNumber: number,
+                            email: email
+                            };
+        this.data.push(newContact);
+    }
+};
+
+contactBook.addContact('Joe', '07986', 'kent4@gmail.com');
+console.log(contactBook.searchContact(prompt('Search for contact')));
