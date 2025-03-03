edf6edd650e12467896b5edce62b89f4248b15fe
vladimirkuznietsov <vladimir.kuznietsov.02@gmail.com>
2025-03-03 20:01:13 +0200

    hw14/fix

--- homework_14/js/task_1.js
+++ homework_14/js/task_1.js
@@ -1,12 +1,49 @@
 const images = [
     'https://cdn.pixabay.com/photo/2025/02/26/09/50/river-9432587_1280.jpg',
     'https://cdn.pixabay.com/photo/2025/01/22/16/43/countryside-9352596_1280.jpg',
-    'https://cdn.pixabay.com/photo/2025/02/21/11/06/woman-9421843_1280.jpg'
+    'https://cdn.pixabay.com/photo/2025/02/21/11/06/woman-9421843_1280.jpg',
+    'https://cdn.pixabay.com/photo/2025/02/15/21/04/house-9409916_1280.jpg'
 ]
 
 const nextBtn = document.getElementById('next');
 const prevBtn = document.getElementById('previous');
 let currentImg = document.getElementById('current-img');
+const dotsContainer = document.getElementById('dots');
+
+images.forEach((image, index) => {
+    let dot = document.createElement('div');
+    dot.className = 'dot';
+    if (index === 0) dot.classList.add('active');
+    dot.addEventListener('click', () => {
+        currentImg.src = images[index];
+        updateDots(index);
+        updateButtons();
+    });
+    dotsContainer.appendChild(dot);
+});
+
+const updateDots = (index) => {
+    let dots = document.getElementsByClassName('dot');
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('active');
+    }
+    dots[index].classList.add('active');
+};
+
+const updateButtons = () => {
+    const currentIndex = images.indexOf(currentImg.src);
+    if (currentIndex === 0) {
+        prevBtn.style.display = 'none';
+    } else {
+        prevBtn.style.display = 'block';
+    }
+
+    if (currentIndex === images.length - 1) {
+        nextBtn.style.display = 'none';
+    } else {
+        nextBtn.style.display = 'block';
+    }
+};
 
 nextBtn.addEventListener('click', () => {
     if(images.indexOf(currentImg.src) !== images.length - 1) {
@@ -33,3 +70,4 @@ prevBtn.addEventListener('click', () => {
     }
 });
 
+updateButtons();
